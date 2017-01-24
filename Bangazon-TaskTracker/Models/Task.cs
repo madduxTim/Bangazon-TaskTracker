76d9fd47680736614aac28e6782e4b4cc5b0ce2d
Tim Maddux <timothymaddux@gmail.com>
2017-01-23 18:31:46 -0600

    Initial setup

---
+++ Bangazon-TaskTracker/Models/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bangazon_TaskTracker.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public eStatus Status { get; set; }
+        public DateTime CompletedOn { get; set; }
+        public enum eStatus
+        {
+            ToDo,
+            InProgress,
+            Complete
+        }
+    }
+}
