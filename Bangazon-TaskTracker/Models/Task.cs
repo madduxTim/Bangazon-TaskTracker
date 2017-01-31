ceb293cc415520c868933fb3687c0519ff823ce1
Tim Maddux <timothymaddux@gmail.com>
2017-01-30 20:38:41 -0600

    Near all API methods working

--- Bangazon-TaskTracker/Models/Task.cs
+++ Bangazon-TaskTracker/Models/Task.cs
@@ -9,9 +9,12 @@ namespace Bangazon_TaskTracker.Models
     public class Task
     {
         [Key]
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Required]
         public eStatus Status { get; set; }
         public DateTime CompletedOn { get; set; }
         public enum eStatus
